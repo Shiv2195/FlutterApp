931704ead4d7ef905a28fa634e7617c6ff948fd0
root <root@localhost.localdomain>
2019-07-22 20:00:24 +0530

    ListView Added

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import './products_manager.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Easy List"),
+        ),
+        body: ProductManager(),
+      ),
+    );
+  }
+}
