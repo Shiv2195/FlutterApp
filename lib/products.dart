65a3cf5a7e8a1700e07a3d3d83beea509ee7b9d1
root <root@localhost.localdomain>
2019-07-31 19:54:14 +0530

    routes added

--- lib/products.dart
+++ lib/products.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+
 import './pages/product.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, String>> products;
+  final Function deleteProduct;
 
-  Products([this.products = const []]);
+  Products(this.products, {this.deleteProduct}) {
+    print('[Products Widget] Constructor');
+  }
 
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
       child: Column(
         children: <Widget>[
-          Image.asset(products[index]['imageUrl']),
+          Image.asset(products[index]['image']),
           Text(products[index]['title']),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               FlatButton(
                 child: Text('Details'),
-                onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ProductPage(
-                            products[index]['title'],
-                            products[index]['imageUrl']))),
+                onPressed: () => Navigator
+                        .pushNamed<bool>(
+                            context, '/product/' + index.toString())
+                        .then((bool value) {
+                      if (value) {
+                        deleteProduct(index);
+                      }
+                    }),
               )
             ],
           )
@@ -46,6 +52,7 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('[Products Widget] build()');
     return _buildProductList();
   }
 }
